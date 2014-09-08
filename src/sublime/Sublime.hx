823a4f8dcef58e8c49cfe784fa2062e00e0232a6
Dan Korostelev <nadako@gmail.com>
2014-09-09 01:38:56 +0400

    some more externs

--- src/sublime/Sublime.hx
+++ src/sublime/Sublime.hx
@@ -4,4 +4,7 @@ package sublime;
 extern class Sublime {
     static var INHIBIT_EXPLICIT_COMPLETIONS(default,null):Int;
     static var INHIBIT_WORD_COMPLETIONS(default,null):Int;
+
+    static function set_timeout(callback:Void->Void, delay:Int):Void;
+    static function windows():Array<Window>;
 }
--- src/sublime/plugin/EventListener.hx
+++ src/sublime/plugin/EventListener.hx
@@ -6,4 +6,6 @@ import sublime.View;
 @:pythonImport("sublime_plugin", "EventListener")
 extern class EventListener {
     function on_query_completions(view:View, prefix:String, locations:Array<Int>):Tup2<Array<Tup2<String,String>>, Int>;
+    function on_activated(view:View):Void;
+    function on_deactivated(view:View):Void;
 }
